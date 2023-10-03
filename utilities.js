c11d727e3115b2e818aede511bd9e2b5cce00ea5
Jose F Rivas C <josefelixrc7@gmail.com>
2023-10-03 19:39:50 -0400

    feat: Improve utilities

--- utilities.js
+++ utilities.js
@@ -1,16 +1,44 @@
+const axios = require('axios');
+const wl = require('./woodpecker_login');
+
+const start_message = function(name)
+{
+    console.log(``);
+    console.log(`----------------- ${name}`);
+    console.log(``);
+};
+
+const end_message = function(name)
+{
+    console.log(``);
+    console.log(`----------------- ${name}`);
+    console.log(``);
+};
 
 exports.utilities = 
 {
-    start_message: function(name)
+    start_message: start_message
+    ,end_message: end_message
+    ,request : (test_name, json_data, target, callback = function(){}) => wl.login(function(sid)
     {
-        console.log(``);
-        console.log(`${name}-----------------`);
-        console.log(``);
-    }
-    ,end_message: function(name)
-    {
-        console.log(``);
-        console.log(`-----------------${name}`);
-        console.log(``);
-    }
+        start_message(test_name);
+    
+        axios.get(`https://127.0.0.1:8080/api/${target}?json=${JSON.stringify(json_data)}`, {headers:
+        {
+            'Cookie': `cpw-woodpecker-sid=${sid}`
+            ,'Content-Type': 'application/json'
+        }})
+        .then(res => 
+        {
+            console.log(res.data);
+            end_message(test_name);
+            callback();
+        })
+        .catch(err =>
+        {
+            console.error(err.response.data)
+            end_message(test_name);
+        })
+        ;
+    })
 };
