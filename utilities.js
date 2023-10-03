92ad88d51967553502b26664596357f1a7898c55
Jose F Rivas C <josefelixrc7@gmail.com>
2023-10-03 14:56:57 -0400

    feat: Add utilities

---
+++ utilities.js
@@ -0,0 +1,16 @@
+
+exports.utilities = 
+{
+    start_message: function(name)
+    {
+        console.log(``);
+        console.log(`${name}-----------------`);
+        console.log(``);
+    }
+    ,end_message: function(name)
+    {
+        console.log(``);
+        console.log(`-----------------${name}`);
+        console.log(``);
+    }
+};
