b5d196e60bd029b0ae808c32458e873d71a3d296
Jose F Rivas C <josefelixrc7@gmail.com>
2023-10-03 19:39:34 -0400

    feat: Add more tests to IqualsFilter tests

--- iquals_filter_tests.js
+++ iquals_filter_tests.js
@@ -1,27 +1,9 @@
-const axios = require('axios');
 
 const ut = require('./utilities');
-const wl = require('./woodpecker_login');
 
-wl.login(function(sid)
-{
-    ut.utilities.start_message('Test 1');
 
-    let json = [{"type":"iqual","contents":[{"id":"0","value":2}]}];
-    axios.get(`https://127.0.0.1:8080/api/stores?json=${JSON.stringify(json)}`, {headers:
-    {
-        'Cookie': `cpw-woodpecker-sid=${sid}`
-        ,'Content-Type': 'application/json'
-    }})
-    .then(res => 
-    {
-        console.log(res.data);
-        ut.utilities.end_message('Test 1');
-    })
-    .catch(err =>
-    {
-        console.error(err.response.data)
-        ut.utilities.end_message('Test 1');
-    })
-    ;
-});
+const test3 = () => ut.utilities.request('Test 3', [{"type":"iqual","contents":[{"id":"0","value":2}]}], 'stores');
+
+const test2 = () => ut.utilities.request('Test 2', [{"type":"iqual","contents":[{"id":"0","value":2}]}], 'products', test3);
+
+ut.utilities.request('Test 1', [{"type":"iqual","contents":[{"id":"0","value":3}]}], 'stores', test2);
