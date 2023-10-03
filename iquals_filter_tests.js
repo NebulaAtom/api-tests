d9ca5c0c98c7c1fb39a0b2f791bc5181637df2d6
Jose F Rivas C <josefelixrc7@gmail.com>
2023-10-03 14:57:14 -0400

    Use utilities in Iquals filter test

--- iquals_filter_tests.js
+++ iquals_filter_tests.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
 
+const ut = require('./utilities');
 const wl = require('./woodpecker_login');
 
 wl.login(function(sid)
 {
-    let json = [];
+    ut.utilities.start_message('Test 1');
+
+    let json = [{"type":"iqual","contents":[{"id":"0","value":2}]}];
     axios.get(`https://127.0.0.1:8080/api/stores?json=${JSON.stringify(json)}`, {headers:
     {
         'Cookie': `cpw-woodpecker-sid=${sid}`
@@ -13,7 +16,12 @@ wl.login(function(sid)
     .then(res => 
     {
         console.log(res.data);
+        ut.utilities.end_message('Test 1');
     })
-    .catch(err => console.error(err.response.data))
+    .catch(err =>
+    {
+        console.error(err.response.data)
+        ut.utilities.end_message('Test 1');
+    })
     ;
 });
