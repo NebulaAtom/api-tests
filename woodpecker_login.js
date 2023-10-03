4cc557526b8d0c2159568b6e55e07f3b20a04b5d
Jose F Rivas C <josefelixrc7@gmail.com>
2023-10-03 12:30:21 -0400

    feat: Add CPW Woodpecker login system

---
+++ woodpecker_login.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const login = function(callback)
+{
+    axios.post('https://127.0.0.1:8080/api/v0/system/login', [{"user": "root", "password": "root_password"}])
+    .then(res => search_sid(res, callback))
+    .catch(err => console.error(`Error al iniciar sesion: ${err}`))
+    ;
+}
+
+const search_sid = function(response, callback)
+{
+    let cookies = response.headers['set-cookie'];
+
+    for(let cookie of cookies)
+    {
+        let first = cookie.split(';');
+        let session = first[0].split('=');
+        if(session[0] == 'cpw-woodpecker-sid')
+            callback(session[1]);
+    }
+
+};
+
+login(function(sid)
+{
+    console.log(sid);
+});
