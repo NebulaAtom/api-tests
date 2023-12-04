2d45e5f4fb855245d913876e1bdeb571717dfa6f
Jose F Rivas C <josefelixrc7@gmail.com>
2023-12-04 12:40:12 -0400

    feat: Delete old files

--- iquals_filter_tests.js
+++
@@ -1,9 +0,0 @@
-
-const ut = require('./utilities');
-
-
-const test3 = () => ut.utilities.request('Test 3', [{"type":"iqual","contents":[{"id":"0","value":2}]}], 'stores');
-
-const test2 = () => ut.utilities.request('Test 2', [{"type":"iqual","contents":[{"id":"0","value":2}]}], 'products', test3);
-
-ut.utilities.request('Test 1', [{"type":"iqual","contents":[{"id":"0","value":3}]}], 'stores', test2);
